f826229d0f8756496be945717d2a5f34824bcaa2
DevDyna <andrearisorti2003@gmail.com>
2025-05-27 00:24:19 +0200

    added blazerod block

--- kubejs/startup_scripts/customItemCreation.js
+++ kubejs/startup_scripts/customItemCreation.js
@@ -1,7 +1,14 @@
-StartupEvents.registry('item', event => {
-    //kubejs/assets/kubejs/textures/item/tech_token.png
-    event.create('techacademy:tech_token').displayName('Tech Token')
-    event.create('techacademy:ore_dictionary').displayName('Ore Dictionary').maxStackSize(1)
+StartupEvents.registry("item", (event) => {
+  //kubejs/assets/kubejs/textures/item/tech_token.png
+  event.create("techacademy:tech_token").displayName("Tech Token");
+  event
+    .create("techacademy:ore_dictionary")
+    .displayName("Ore Dictionary")
+    .maxStackSize(1)
     .tooltip({ translate: "item.techacademy.ore_dictionary.tooltip1" })
-    .tooltip({ translate: "item.techacademy.ore_dictionary.tooltip2" })
-  })
+    .tooltip({ translate: "item.techacademy.ore_dictionary.tooltip2" });
+});
+
+StartupEvents.registry("block", (event) => {
+  event.create("techacademy:blazerod_block");
+});
