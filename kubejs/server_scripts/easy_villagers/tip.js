d694a740d4ca263c488250524afe3d67c071957f
DevDyna <andrearisorti2003@gmail.com>
2025-05-26 22:41:31 +0200

    added simple traslable tooltip (NYC)

---
+++ kubejs/server_scripts/easy_villagers/tip.js
@@ -0,0 +1,4 @@
+ItemEvents.modifyTooltips(event=>{
+    event.add("easy_villagers:villager",{ translate: "item.techacademy.easy_villagers.desc" })
+    event.add('easy_piglins:piglin',{ translate: "item.techacademy.easy_piglins.desc" })
+})
