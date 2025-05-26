3b2ef244199ae82b588c7a9237ecff83ca4c2e2b
DevDyna <andrearisorti2003@gmail.com>
2025-05-26 22:55:22 +0200

    separated easy mods

--- kubejs/server_scripts/easy_villagers/tip.js
+++ kubejs/server_scripts/easy_villagers/tip.js
@@ -1,4 +1,3 @@
 ItemEvents.modifyTooltips(event=>{
     event.add("easy_villagers:villager",{ translate: "item.techacademy.easy_villagers.desc" })
-    event.add('easy_piglins:piglin',{ translate: "item.techacademy.easy_piglins.desc" })
 })
