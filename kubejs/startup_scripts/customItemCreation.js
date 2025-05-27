51de6408845be804a37a5c705f0c7d9052a5534c
DevDyna <andrearisorti2003@gmail.com>
2025-05-27 12:09:18 +0200

    removed duplicated fields with same result

--- kubejs/startup_scripts/customItemCreation.js
+++ kubejs/startup_scripts/customItemCreation.js
@@ -1,9 +1,9 @@
 StartupEvents.registry("item", (event) => {
-  //kubejs/assets/kubejs/textures/item/tech_token.png
-  event.create("techacademy:tech_token").displayName("Tech Token");
-  event
-    .create("techacademy:ore_dictionary")
-    .displayName("Ore Dictionary")
+  //kubejs auto-generate display names based on registry ids
+  event.create("techacademy:tech_token")
+  //.displayName("Tech Token");
+  event.create("techacademy:ore_dictionary")
+    // .displayName("Ore Dictionary")
     .maxStackSize(1)
     .tooltip(Component.translatable("item.techacademy.ore_dictionary.tooltip1"))
     .tooltip(
