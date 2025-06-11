6ab61d28d40a340564d8ce8365daf75088d4f938
DevDyna <andrearisorti2003@gmail.com>
2025-06-11 14:09:20 +0200

    meka infusions compressed

---
+++ kubejs/server_scripts/mekanism/qol/infusion.js
@@ -0,0 +1,41 @@
+ServerEvents.recipes((event) => {
+  //bronze
+  event.recipes.mekanism.metallurgic_infusing(
+    "36x mekanism:ingot_bronze",
+    "3x #immersiveengineering:blocks/copper",
+    "90x mekanism:tin",
+    false
+  );
+
+  //alloy
+  event.recipes.mekanism.metallurgic_infusing(
+    "9x mekanism:alloy_infused",
+    "#immersiveengineering:blocks/copper",
+    "90x mekanism:redstone",
+    false
+  );
+
+  //circuit
+  event.recipes.mekanism.metallurgic_infusing(
+    "9x mekanism:basic_control_circuit",
+    "#c:storage_blocks/osmium",
+    "180x mekanism:redstone",
+    false
+  );
+
+  //electrum
+  event.recipes.mekanism.metallurgic_infusing(
+    "9x immersiveengineering:dust_electrum",
+    "gold_block",
+    "90x mekanism:redstone",
+    false
+  );
+
+  //create rose quartz
+  event.recipes.mekanism.metallurgic_infusing(
+    "4x create:rose_quartz",
+    "quartz_block",
+    "40x mekanism:redstone",
+    false
+  );
+});
