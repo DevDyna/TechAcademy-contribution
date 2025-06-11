ee416f66acec357f1bb9019bb9f051f282a3eff9
DevDyna <andrearisorti2003@gmail.com>
2025-06-11 14:09:31 +0200

    Create compatUtils.js

---
+++ kubejs/server_scripts/customs/compatUtils.js
@@ -0,0 +1,107 @@
+// priority: 10
+
+/**
+ * ## Custom AdvancedAE2 function
+ *
+ * @param {number} energy total energy used foreach recipe
+ * @param {string} fluid fluid id
+ * @param {number} fluid_amount
+ * @param {[{id:string,amount:number}]} items
+ *
+ * when id start with `#` it become an item tag
+ *
+ * @param {string} output_type
+ *
+ * `ae2:i` -> item
+ *
+ * `ae2:f` -> fluid?
+ *
+ * ?? -> ?? (need more info)
+ *
+ * @param {string} output type id
+ * @param {number} output_amount cannot be over 64
+ * @returns {object} recipe json for `event.custom()`
+ */
+function reactionChamber(
+  energy,
+  fluid,
+  fluid_amount,
+  items,
+  output_type,
+  output,
+  output_amount
+) {
+  let item_inputs = [];
+
+  items.forEach((it) => {
+    item_inputs.push(
+      it.id.startsWith("#")
+        ? { amount: it.amount, ingredient: { tag: it.id.slice(1) } }
+        : { amount: it.amount, ingredient: { item: it.id } }
+    );
+  });
+
+  return {
+    type: "advanced_ae:reaction",
+    input_energy: energy,
+    input_fluid: {
+      amount: fluid_amount,
+      ingredient: {
+        fluid: fluid,
+      },
+    },
+    input_items: item_inputs,
+    output: {
+      "#": output_amount,
+      "#t": output_type,
+      id: output,
+    },
+  };
+}
+
+
+/**
+ * ## Custom ExtendedAE2 function
+ *
+ * @param {string} fluid fluid id
+ * @param {number} fluid_amount
+ * @param {[{id:string,amount:number}]} items
+ *
+ * when id start with `#` it become an item tag
+ *
+ * @param {string} output type id
+ * @param {number} output_amount cannot be over 64
+ * @returns {object} recipe json for `event.custom()`
+ */
+function crystalChamber(
+  fluid,
+  fluid_amount,
+  items,
+  output,
+  output_amount
+) {
+  let item_inputs = [];
+
+  items.forEach((it) => {
+    item_inputs.push(
+      it.id.startsWith("#")
+        ? { amount: it.amount, ingredient: { tag: it.id.slice(1) } }
+        : { amount: it.amount, ingredient: { item: it.id } }
+    );
+  });
+
+  return {
+    type: "extendedae:crystal_assembler",
+    input_fluid: {
+      amount: fluid_amount,
+      ingredient: {
+        fluid: fluid,
+      },
+    },
+    input_items: item_inputs,
+    output: {
+      count: output_amount,
+      id: output,
+    },
+  };
+}
