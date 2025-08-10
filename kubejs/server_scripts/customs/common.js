ef0f74fef946d8000d64bc98b6abf6cbbf2e3f4a
SiriosDev <26876994+SiriosDev@users.noreply.github.com>
2025-08-10 23:02:20 +0200

    refactor time conversion functions and add direction utilities

--- kubejs/server_scripts/customs/common.js
+++ kubejs/server_scripts/customs/common.js
@@ -1,5 +1,5 @@
 //database of all time operations
-const time = {
+const timeTab = {
   seconds: 20,
   minutes: 20 * 60,
   hours: 20 * 60 * 60,
@@ -21,7 +21,7 @@ const time = {
  *          original ticks if the unit is unrecognized.
  */
 function ticksToTime(ticks, outputTimeType) {
-  return ticks / (time[outputTimeType] || 1);
+  return ticks / (timeTab[outputTimeType] || 1);
 }
 
 /**
@@ -31,6 +31,59 @@ function ticksToTime(ticks, outputTimeType) {
  * @param {string} inputTimeType - The time unit of the input time. Can be one of the following: "seconds", "minutes", "hours", "days", "weeks", "months", "years". If not specified or unrecognized, returns the input time value.
  * @returns {number} - The equivalent time in ticks, or the original time if the unit is unrecognized.
  */
-function timeToTicks(ticks, outputTimeType) {
-  return ticks * (time[outputTimeType] || 1);
+function timeToTicks(time, inputTimeType) {
+  return Math.ceil(time * (timeTab[inputTimeType] || 1));
+}
+
+const inverseDirection = {
+  "north": "south",
+  "south": "north",
+  "west": "east",
+  "east": "west",
+  "down": "up",
+  "up": "down"
+}
+
+/**
+ * Returns the inverse direction of a given direction string, or the original
+ * direction if it is not recognized.
+ *
+ * @param {string} direction - The direction string to be inverted. Can be one
+ *        of the following: "north", "south", "west", "east", "up", "down". If
+ *        not specified or unrecognized, the original direction is returned.
+ * @returns {string} - The inverse direction string, or the original direction
+ *          if it is not recognized.
+ */
+function invertDirection(direction) {
+  return inverseDirection[direction] || direction;
+}
+
+const behindSchema = {
+  "north": [0, 0, -1],
+  "south": [0, 0, 1],
+  "west": [-1, 0, 0],
+  "east": [1, 0, 0],
+  "down": [0, -1, 0],
+  "up": [0, 1, 0]
+}
+
+
+/**
+ * Calculates the position behind a given position based on the specified direction.
+ *
+ * This function uses a predefined schema to determine the offset for each
+ * direction. If the direction is recognized, it adds the corresponding offset
+ * to the provided position. If the direction is not recognized, it returns
+ * the original position.
+ *
+ * @param {string} direction - The direction string indicating the desired
+ *        direction for which to get the position behind. Valid directions
+ *        include: "north", "south", "west", "east", "up", "down".
+ * @param {BlockPos} pos - The current position value.
+ * @returns {BlockPos} - The position value behind the given position based on
+ *          the specified direction, or the original position if the direction
+ *          is not recognized.
+ */
+function getBehindPos(direction, pos) {
+  return BlockPos(pos.getX() + behindSchema[direction][0], pos.getY() + behindSchema[direction][1], pos.getZ() + behindSchema[direction][2]) || pos;
 }
